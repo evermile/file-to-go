2e293e3c29aa3e7e6f8b46d03142de722a737d07
Ynon Cohen <ynon@evermile.io>
2022-04-07 10:36:13 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module file-to-go
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var imports = []string{
+	"import (",
+	"\t\"bytes\"",
+	"\t\"compress/gzip\"",
+	"\t\"encoding/base64\"",
+	"\t\"strings\"",
+	")",
+}
+
+var decodeFunc = []string{
+	"func GetFullSwagger() string {",
+	"	zipped, _ := base64.StdEncoding.DecodeString(strings.Join(swaggerSpec, \"\"))",
+	"	zr, _ := gzip.NewReader(bytes.NewReader(zipped))",
+	"	var buf bytes.Buffer",
+	"	_, _ = buf.ReadFrom(zr)",
+	"	return buf.String()",
+	"}",
+}
+
+func main() {
+	apiFile := flag.String("file", "bla.json", "The file to read")
+	outFile := flag.String("outfile", "bla.go", "The output file to write")
+	pkg := flag.String("package", "main", "The package for the output class")
+
+	flag.Parse()
+	buf := getZippedContent(apiFile)
+	encoded := base64.StdEncoding.EncodeToString(buf.Bytes())
+	if file, err := os.Create(*outFile); err != nil {
+		panic(err)
+	} else {
+		writer := bufio.NewWriter(file)
+		writer.WriteString(fmt.Sprintf("package %s \n\n", *pkg))
+		for _, l := range imports {
+			writer.WriteString(l + "\n")
+		}
+
+		writer.WriteString("var swaggerSpec = []string{\n")
+		writer.WriteString("\t\"")
+		for i, r := range encoded {
+			writer.WriteRune(r)
+			if (i+1)%100 == 0 {
+				writer.WriteString("\", \n\t\"")
+			}
+		}
+		writer.WriteString("\", \n")
+		writer.WriteString("}\n")
+
+		for _, l := range decodeFunc {
+			writer.WriteString(l + "\n")
+		}
+		writer.WriteString("\n")
+
+		writer.Flush()
+	}
+}
+
+func getZippedContent(apiFile *string) bytes.Buffer {
+	var content []byte
+	if file, err := os.Open(*apiFile); err != nil {
+		panic(err)
+	} else {
+		reader := bufio.NewReader(file)
+		if content, err = ioutil.ReadAll(reader); err != nil {
+			panic(err)
+		}
+	}
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
+	defer func(zw *gzip.Writer) {
+		_ = zw.Close()
+	}(zw)
+	if _, err := zw.Write(content); err != nil {
+		panic(err)
+	}
+	_ = zw.Flush()
+	return buf
+}
